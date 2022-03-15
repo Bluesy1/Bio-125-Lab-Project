e82e5fd1aa0d47b5a0d875c940391ba98580c1c6
Bluesy <68259537+Bluesy1@users.noreply.github.com>
2022-03-15 11:30:04 -0700

    Delete test.R

--- test.R
+++
@@ -1,7 +0,0 @@
-library(readr)
-library(dplyr)
-Data <- read_csv("processed_data/Kendal-Freedman_BIOL125_Mung_Processed-Data.csv", show_col_types = FALSE)
-
-results <- t.test(Length_mm ~ Treatment_pH, data = Data, var.equal = TRUE, conf.level = 0.95)
-
-results
